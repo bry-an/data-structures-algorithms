b09d01d6178675543efa1229d548c9019546695d
bry-an <bbyunis@gmail.com>
2019-04-07 23:11:48 -0600

    add nextPermutation

---
+++ javascript/algorithms/nextPermutation.js
@@ -0,0 +1,29 @@
+function generatePermutations (arr) {
+    let result = []
+    function permute (arr, m = []) {
+      if (arr.length === 0) {
+        result.push(parseInt(m.join("")))
+      } else {
+        for (let i = 0; i < arr.length; i++) {
+          let curr = arr.slice()
+          let next = curr.splice(i, 1)
+          permute(curr.slice(), m.concat(next))
+       }
+     }
+   }
+   permute(arr)
+   return result
+  }
+generatePermutations([1,2,3,4])
+
+function nextPermutation(num) {
+    const permutations = generatePermutations(num.toString().split(""))
+    let smallest = permutations[0]
+
+    for (let i=0; i < permutations.length; i++) {
+        if (permutations[i] > num && permutations[i] < smallest) {
+            smallest = permutations[i]
+        }
+    }
+    return smallest
+}
