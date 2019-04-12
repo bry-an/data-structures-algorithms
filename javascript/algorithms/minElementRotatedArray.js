17b3b184be0ede86213dcd6a71b8caf8b9f836e1
bry-an <bbyunis@gmail.com>
2019-04-11 20:57:23 -0600

    add comments to minElementRotatedArray js solution

--- javascript/algorithms/minElementRotatedArray.js
+++ javascript/algorithms/minElementRotatedArray.js
@@ -1,18 +1,18 @@
 function minElement(arr) {
     if (arr.length === 2 && arr[0] > arr[1]) {
-        return arr[1]
+        return arr[1] // we know this array contains the pivot point and since it only has two out-of-order elements, the second (smaller) must be the smallest
     }
     const mid = Math.ceil(arr.length/2) // must be O(log N) so need to divide N in half on each iteration of recursion
-    const arr1 = arr.slice(0, mid)
-    const arr2 = arr.slice(mid)
+    const left = arr.slice(0, mid)
+    const right = arr.slice(mid)
 
-    if (arr1[0] > arr1[arr1.length-1]) { // we know pivot happens in this half
-        return minElement(arr1)
+    if (left[0] > left[left.length-1]) { // we know pivot (thus the smallest #) happens in this half
+        return minElement(left)
     }
-    else if (arr2[0] > arr2[arr2.length-1]) { // we know pivot happens in this half
-        return minElement(arr2)
+    else if (right[0] > right[right.length-1]) { // we know pivot (thus the smallest #) happens in this half
+        return minElement(right)
     }
-    return arr2[0]
+    return right[0]
 }
 
-console.log(minElement([16, 17, 18, 19, 11])) // 11
+console.log(minElement([5, 7, 10, 3, 4])) // 3
