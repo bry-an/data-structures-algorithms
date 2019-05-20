304c8acd13bfdbede3a2d4796f178610e10bd154
Bryan Yunis <bbyunis@gmail.com>
2019-05-19 22:26:48 -0600

    clean up logs

--- javascript/algorithms/circleChain.js
+++ javascript/algorithms/circleChain.js
@@ -1,22 +1,18 @@
 function circle(arr, word = arr[0], results = []) {
     if (arr.length === 0) {
-        console.log("boom finish")
     }
     console.log("calling circle", arr, word, results)
     const arrCopy = [...arr]
     const removed = arrCopy.splice(arrCopy.indexOf(word), 1)
     results.push(removed[0])
-    console.log("arrcopy", arrCopy)
 
     if (arrCopy.length) {
-        console.log('length')
 
 
         const lastLetter = word[word.length - 1]
 
         arrCopy.forEach(item => {
             if (item[0] === lastLetter) {
-                console.log("boom", item, lastLetter)
                 return circle(arrCopy, item, results)
 
             }
