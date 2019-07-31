8c32df695a45394925c70b0a025588e04c3af87e
Bryan Yunis <bbyunis@gmail.com>
2019-07-30 23:23:54 -0600

    add javascript imp of java.String.hashCode() fn

---
+++ javascript/data-structures/javaHashCodeFn.js
@@ -0,0 +1,15 @@
+// implementation of Java's String.hashCode(), borrowed from:
+// https://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+
+String.prototype.hashCode = function () {
+    var hash = 0;
+    if (this.length == 0) {
+        return hash;
+    }
+    for (var i = 0; i < this.length; i++) {
+        var char = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
