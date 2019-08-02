55a9ab7fb43b0593b04e6c2cbfeb50f28f1b8c15
Bryan Yunis <bbyunis@gmail.com>
2019-08-02 06:09:32 -0600

    add search method functional bst

--- javascript/data-structures/binary-tree-functional.js
+++ javascript/data-structures/binary-tree-functional.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 
-const Cons = function(head, left, right) {
+const Cons = function (head, left, right) {
   this.head = head;
   this.left = left;
   this.right = right;
@@ -12,13 +12,13 @@ const Nil = {
   isEmpty: true,
 
   getHead() {
-    throw new Error("Cannot access head element on empty list");
+    throw new Error("Cannot access head element on empty tree");
   },
   getLeft() {
-    throw new Error("Cannot access left element on empty list");
+    throw new Error("Cannot access left element on empty tree");
   },
   getRight() {
-    throw new Error("Cannot access right element on empty list");
+    throw new Error("Cannot access right element on empty tree");
   }
 };
 
@@ -26,21 +26,57 @@ const cell = (head, left, right) => {
   return new Cons(head, left, right);
 };
 
-const addElement = (list, element) => {
-  if (list.isEmpty) {
+const addElement = (tree, element) => {
+  if (tree.isEmpty) {
     return cell(element, Nil, Nil);
   }
-  if (element > list.head) {
-    return cell(list.head, list.left, addElement(list.right, element));
+  if (element > tree.head) {
+    return cell(tree.head, tree.left, addElement(tree.right, element));
   }
-  if (element < list.head) {
-    return cell(list.head, addElement(list.left, element), list.right);
+  if (element < tree.head) {
+    return cell(tree.head, addElement(tree.left, element), tree.right);
   }
   return;
 };
 
-const myTree = cell(5, Nil, Nil);
+const treebuilder = (elements, tree = Nil, index = 0) => {
 
-const newTree = addElement(myTree, 6); //?
-const anotherTree = addElement(newTree, 4); //?
-addElement(anotherTree, 7); //?
+  if (!elements || !elements[index]) {
+    return tree
+  }
+
+  return treebuilder(elements, addElement(tree, elements[index]), index + 1)
+}
+
+
+const search = (tree, element) => {
+  if (!tree || tree.isEmpty) {
+    return false
+  }
+  if (tree.head === element) {
+    return true
+  }
+  if (tree.head > element) {
+    return search(tree.left, element)
+  }
+  else {
+    return search(tree.right, element)
+  }
+
+  return false
+}
+
+
+
+// tests
+const evergreen = treebuilder([5, 8, 21, 15, 4])
+assert.equal(search(evergreen, 8), true)
+assert.equal(search(evergreen), false)
+
+
+
+
+
+
+
+
